18890b2df3bb2f91fc4adb48f3b499f7a89399da
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-11-22 16:25:30 +0100

    websocket frontend

---
+++ src/components/Chat.vue
@@ -0,0 +1,45 @@
+<script setup>
+ //import ref, reactive
+    import { ref, reactive, onMounted } from 'vue';
+
+    let messages = ref(['yo', 'yoyo']);
+    let newMessage = ref('');
+    let socketServer = null;
+
+    onMounted(() => {
+        socketServer = new WebSocket('ws://localhost:3000/primus');
+        //listen for data from server
+        socketServer.onmessage = (event) => {
+            let data = JSON.parse(event.data);
+            if (data.action === 'newMessage') {
+                messages.value.push(data.message);
+            }
+        }
+    });
+
+    const sendMessage = () => {
+        let m = {
+            "action": "newMessage",
+            "message": newMessage.value
+        }
+
+        socketServer.send(JSON.stringify(m));
+    }
+</script>
+
+<template>
+  <div>
+    <h1>Chat</h1>
+    <ul>
+        <li v-for="m in messages">{{ m }}</li>
+    </ul>
+    <div>
+        <input type="text" v-model="newMessage"/>
+        <button @click="sendMessage">Send</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
